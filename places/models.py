dfb59ec2d85dd7fe408b286fc3cb1d5231215254
mostafa <199765106+mostafaahmand@users.noreply.github.com>
2026-08-19 08:13:28 -0700

    Add PostgreSQL database integration

--- places/models.py
+++ places/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Place(models.Model):
     title = models.CharField('عنوان',max_length=100)
-    description = models.TextField('توضیحات',blank=True,)    
+    description = models.TextField('توضیحات',blank=True)    
     image = models.ImageField('تصویر مکان', upload_to='places/',blank=True,null=True)
     date = models.DateTimeField('تاریخ ثبت', auto_now_add=True)
 
